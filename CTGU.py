32ad96b5c5f872b0633378c898a7f95019dd36c6
1213清心 <30729159+szh1213@users.noreply.github.com>
2018-05-03 16:02:48 +0800

    Update CTGU.py

--- CTGU.py
+++ CTGU.py
@@ -10,10 +10,8 @@ mylist=[]
 name = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D',\
              'E','F','G','H','i','J','K','M','N','P','Q','R','S','T',\
              'U','W','X','Y','Z']
-#userName=input('输入学号：')
-#passWord=input('输入密码：')
-userName='2016105405'
-passWord='425451'
+userName=input('输入学号：')
+passWord=input('输入密码：')
 userKey=''
 svip = md5(bytes('szh1213_20180101'.encode('utf-8')))
 svip.update(userName[:].encode('utf-8'))
